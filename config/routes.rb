46d4250aea6a2dffb5fa0e97188fb83e5b72ddae
Tyler Stuart <tstuart@1on1.com>
2012-10-03 15:38:16 -0600

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Reviewer::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   resources :restaurants
 
   root            :to => 'static_pages#home' 
@@ -7,6 +8,8 @@ Reviewer::Application.routes.draw do
   match "/about", :to => 'static_pages#about'
   match "/restaurant", :to => 'restaurants#index'
   match "/signup", to: 'users#new'
+  match "/signin", to: 'sessions#new'
+  match "/signout", to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
